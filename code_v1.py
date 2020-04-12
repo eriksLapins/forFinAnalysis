e9c9a0ff719c1ff416721920a6336267ce07911c
lapinslv <eeryks@gmail.com>
2020-04-12 19:19:31 +0300

    importing data stuff

--- code_v1.py
+++ code_v1.py
@@ -5,5 +5,19 @@ Created on Sun Apr 12 17:36:24 2020
 @author: User
 """
 
+import os
+os.getcwd()
+os.chdir("C:/Users/User/Investment/W_Report/Python/Projects/forFinAnalysis")
 
-import Pandas as pd
+# import packages for analysis
+
+import pandas as pd
+
+# import data as a data frame
+
+fb = pd.DataFrame.from_csv('data/facebook.csv')
+ms = pd.DataFrame.from_csv('data/microsoft.csv')
+
+# show the type of data
+
+print(type(fb)) # .DataFrame shows that it is a data frame
